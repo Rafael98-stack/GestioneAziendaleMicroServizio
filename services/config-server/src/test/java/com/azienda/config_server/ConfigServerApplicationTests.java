4e848991fb9293916ca18634bd7f4218a4b124d5
Friggitello <fggabrielito@gmail.com>
2025-02-13 12:34:02 +0100

    a

---
+++ services/config-server/src/test/java/com/azienda/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azienda.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

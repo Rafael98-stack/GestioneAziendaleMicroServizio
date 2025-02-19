49ee9c1e138d5711727f43e387c952ba04a0eb32
Nanda Pellegrini <pellegrininanda96@gmail.com>
2025-02-19 14:43:21 +0100

    Correzzioni

--- services/dipendenti/src/main/java/com/azienda/dipendenti/services/TimbraturaReportJob.java
+++ services/dipendenti/src/main/java/com/azienda/dipendenti/services/TimbraturaReportJob.java
@@ -5,11 +5,16 @@ import com.azienda.dipendenti.controllers.TimbraturaController;
 import com.azienda.dipendenti.entities.Timbratura;
 import com.azienda.dipendenti.repositories.TimbraturaRepository;
 import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 
+import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
@@ -17,7 +22,7 @@ import java.util.List;
 public class TimbraturaReportJob implements Job
 {
     @Autowired
-    
+    JavaMailSender mailSender;
     @Autowired
     TimbraturaRepository timbraturaRepository;
     @Autowired
@@ -38,12 +43,29 @@ public class TimbraturaReportJob implements Job
 
         try {
             String filePath = csvGeneratorService.generateCsv(timbrature);
-            emailService.sendEmailWithAttachment("pellegrininanda96@gmail.com", filePath);
+            sendEmailWithAttachment("pellegrininanda96@gmail.com", filePath);
             System.out.println("Report timbrature inviato con successo!");
         } catch (MessagingException | IOException e) {
             e.printStackTrace();
         }
     }
 
+    public void sendEmailWithAttachment(String to, String filePath) throws MessagingException
+    {
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setTo(to);
+        helper.setSubject("Report Timbrature Giornaliero");
+        helper.setText("In allegato il report delle timbrature giornaliere.");
+
+        FileSystemResource file = new FileSystemResource(new File(filePath));
+        helper.addAttachment(file.getFilename(), file);
+
+        mailSender.send(message);
     }
-}
+    }
+
+
+
